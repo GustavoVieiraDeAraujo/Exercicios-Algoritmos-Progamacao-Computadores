4896ae3853aa70a29711e45dc90d73139e112b2e
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-08-30 18:41:33 -0300

    Corrigido arredondamento de jogadas na Questao 8

--- Condicionais e Recursividade/Questão 8/Resposta 8 (marcada).py
+++ Condicionais e Recursividade/Questão 8/Resposta 8 (marcada).py
@@ -1,16 +1,20 @@
 def jogadas(a,b):
     if a==b:
         print("0")
-    elif a>b: 
+    elif a>b:
         diferença=a-b
         x=diferença//10
         y=diferença%10
-        if y<10:
+        if y==0:
+             print(x)
+        else:
              print(x+1)
     else:
         diferença=b-a
         x=diferença//10
         y=diferença%10
-        if y<10:
+        if y==0:
+             print(x)
+        else:
              print(x+1)
              
