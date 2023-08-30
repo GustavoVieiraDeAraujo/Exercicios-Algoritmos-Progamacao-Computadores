f2efc185220c3bbf5cc07d339b88224380b849b8
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-08-30 19:03:08 -0300

    Corrigido retorno da funcao de empate na Questao 10

--- Condicionais e Recursividade/Questão 10/Resposta 10.py
+++ Condicionais e Recursividade/Questão 10/Resposta 10.py
@@ -1,6 +1,7 @@
 def achiles(distancia):
     if distancia <= 0.5:
-        return print("Foi decidido o empate.")
+        print("Foi decidido o empate.")
+        return 0
     else:
         print(f'Aquiles se aproximou um pouco, mas a tartaruga ainda está {distancia} metros na frente.')
         return achiles(distancia/2)
