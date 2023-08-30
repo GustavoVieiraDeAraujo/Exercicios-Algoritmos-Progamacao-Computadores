d5ce13947eeec3edf5b9d80d9c1421241bc8851d
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-08-30 19:29:21 -0300

    Reescrita a funcao hh_duracao para tratar horarios no mesmo dia

--- Funções com Resultado/Questão 8/Resposta 8 (marcada).py
+++ Funções com Resultado/Questão 8/Resposta 8 (marcada).py
@@ -1,16 +1,10 @@
 def hh_duracao(a,b,c,d,e,f):
-    if a != d:
-        horas=24-a+d
-        minutos=e-b
-        segundos=f-c
-        return horas,minutos,segundos
-    elif a==d and b==e==0 and c==f==0:
-        horas=24
-        minutos=0
-        segundos=0
-        return horas,minutos,segundos
-    elif a==d and b>=0 and c>=0 and e>=0 and f>=0:
-        horas=d-a
-        minutos=e-b
-        segundos=f-c
-        return horas,minutos,segundos
+    inicio = a*3600 + b*60 + c
+    final = d*3600 + e*60 + f
+    duracao = final - inicio
+    if duracao <= 0:
+        duracao += 24*3600
+    horas = duracao // 3600
+    minutos = (duracao % 3600) // 60
+    segundos = duracao % 60
+    return horas, minutos, segundos
