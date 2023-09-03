4b7bdc55724abc14091905c51db05eb2621de49f
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-09-02 22:31:58 -0300

    Reescrita contagem de amigos usando componentes conexos

--- Dicionarios e Tuplas/Questão 9/Resposta 9 (marcada).py
+++ Dicionarios e Tuplas/Questão 9/Resposta 9 (marcada).py
@@ -5,44 +5,27 @@ numero_duplas=int(y)
 
 nome_alunos=input().split()
 
-dic_duplas={}
-indice=1
-for nome in nome_alunos:
-    while indice < numero_alunos:
-        p = nome_alunos.index(nome)
-        juncao = nome+" "+nome_alunos[p+indice]
-        dic_duplas[juncao]=0
-        indice+=1
-    indice=1
-    numero_alunos-=1
+parent = {nome: nome for nome in nome_alunos}
 
-hist=[]
-temp={}
+def find(a):
+    while parent[a] != a:
+        a = parent[a]
+    return a
+
+def union(a, b):
+    ra, rb = find(a), find(b)
+    if ra != rb:
+        parent[ra] = rb
+
 for i in range(numero_duplas):
-    entrada=input()
-    hist.append(entrada)
-    dupla=entrada.split()
-    for chave in dic_duplas:
-        if dupla[0] in chave:
-            dic_duplas[chave]+=1
-        elif dupla[1] in chave:
-            dic_duplas[chave]+=1
+    entrada = input()
+    dupla = entrada.split()
+    union(dupla[0], dupla[1])
 
-for l in hist:
-    temp[l]=dic_duplas[l]
-    
+from collections import Counter
+tamanho_componente = Counter(find(nome) for nome in nome_alunos)
 
-dic_final={}
-for chave in temp.items():
-    for elemento in chave:
-        for teste in elemento.split():
-            if teste in dic_final:
-                pass
-            else:
-                dic_final[teste]=chave[1]
-        break
-
-ordemAlfabetica=sorted(dic_final) 
+ordemAlfabetica = sorted(nome_alunos)
 
 for unidade in ordemAlfabetica:
-    print(f"{unidade} possui {dic_final[unidade]} amigos")
+    print(f"{unidade} possui {tamanho_componente[find(unidade)] - 1} amigos")
