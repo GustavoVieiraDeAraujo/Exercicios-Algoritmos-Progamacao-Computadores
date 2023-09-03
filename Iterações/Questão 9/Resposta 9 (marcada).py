0254ba1a5a3d50e40fc926aa7db5c3b5dda9d422
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-09-02 21:24:52 -0300

    Corrigido controle de combustivel quando o obstaculo custa mais que o tanque

--- Iterações/Questão 9/Resposta 9 (marcada).py
+++ Iterações/Questão 9/Resposta 9 (marcada).py
@@ -3,8 +3,10 @@ capacidade = litros
 caminho = int(input())
 km = 0
 
-while caminho != -1 and litros >= 0:
+while caminho != -1:
     if caminho == 0:
+        if litros < 1:
+            break
         litros -= 1
         km += 1
         caminho = int(input())
@@ -20,16 +22,16 @@ while caminho != -1 and litros >= 0:
             caminho = int(input())
     elif caminho == 2:
         perdeu = int(input())
+        if perdeu > litros:
+            break
         litros -= perdeu
         km += 1
         caminho = int(input())
-    
-    
-if litros == 0:
-    print(km)
-elif litros == -1:
-    print(km-1)
-elif caminho == -1:
+
+
+if caminho == -1:
     print("Lar Deivis lar")
+else:
+    print(km)
 
 
