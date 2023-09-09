65e5c43ec555e4690284b9bf64061fd1bd83a5f4
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-09-09 16:37:41 -0300

    Corrigida inversao dos rotulos de arquivo quase vazio

--- Arquivos/Questão 4/Resposta 4.py
+++ Arquivos/Questão 4/Resposta 4.py
@@ -1,14 +1,15 @@
 arq_1,arq_2 = input().split()
 
 with open(arq_1) as file:
-    file = file.readlines()
-    if len(file) > 1:
-        print(f"O arquivo {arq_2} esta quase vazio, mas o {arq_1} nao esta!")
+    conteudo_1 = file.read()
+
+if len(conteudo_1) == 1:
+    print(f"O arquivo {arq_1} esta quase vazio, mas o {arq_2} nao esta!")
+    with open(arq_2) as file:
         for line in file:
             print(line.rstrip("\n"))
-    elif len(file) == 1:
-        print(f"O arquivo {arq_1} esta quase vazio, mas o {arq_2} nao esta!")
-        with open(arq_2) as file:
-            for line in file:
-                print(line.replace("\n", ""))
+else:
+    print(f"O arquivo {arq_2} esta quase vazio, mas o {arq_1} nao esta!")
+    for line in conteudo_1.splitlines():
+        print(line)
               
